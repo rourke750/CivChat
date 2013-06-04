3797394b57004236da0ad70e01bc04049fbf2a57
Rourke750 <Rourke7501@gmail.com>
2013-06-03 22:07:09 -0400

    Fixed commands

--- src/com/untamedears/civchat/Commands.java
+++ src/com/untamedears/civchat/Commands.java
@@ -15,31 +15,36 @@ public class Commands implements CommandExecutor{
 	CivChat chat= new CivChat();
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
 		
-             Player playerreciever= Bukkit.getPlayerExact(args[0]);
+             
 		if (label.equalsIgnoreCase("tell") || label.equalsIgnoreCase("message")){
+			
 			if (args.length < 1) {
                 sender.sendMessage("Provide a player name");
-                return false;
+                return true;
         }
-			if (args.length==1){
-				sender.sendMessage("Provide a message");
-			}
+			
 		
-			if (playerreciever.isOnline()==false){
-				sender.sendMessage("Player is offline");
-				return true;
-			}
-			StringBuilder argsmessage= new StringBuilder();
-			for (int i= 1;i<args.length;i++){
-				argsmessage.append(args[i]);
-				argsmessage.append(" ");
-			}
-			argsmessage.toString();
 			
-			
+			if (args.length>=1){
+				Player playerreciever= Bukkit.getPlayerExact(args[0]);
+				
+				if (playerreciever==null){
+					sender.sendMessage("Player is offline");
+					return true;
+				}
+				else{
+				StringBuilder argsmessage= new StringBuilder();
+				for (int i= 1;i<args.length;i++){
+					argsmessage.append(args[i]);
+					argsmessage.append(" ");
+				}
+				argsmessage.toString();
 			sender.sendMessage(ChatColor.RED+"To "+playerreciever.getDisplayName()+": "+ argsmessage);
 			playerreciever.sendMessage(ChatColor.RED+"From "+sender.getName()+": "+ argsmessage);
 			ch.setChannel(sender.getName(), playerreciever.getName(), true);
+			return true;
+				}
+			}
 			return true;
 		}
 		if (label.equalsIgnoreCase("civchat")){
