1b9654df61ad059d375bfe94fc9dc012f918c5a9
shadowjay1 <jrdn.ash@gmail.com>
2013-06-04 22:53:46 -0500

    Tons of fixes!

--- src/com/untamedears/civchat/ChatManager.java
+++ src/com/untamedears/civchat/ChatManager.java
@@ -1,88 +1,102 @@
 package com.untamedears.civchat;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Set;
 
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
-import org.bukkit.configuration.Configuration;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 
 public class ChatManager {
-	private FileConfiguration config_;
-	public ChatManager (FileConfiguration configInstance){config_=configInstance;}
-	int chatmax=config_.getInt("chat.range", 1000);
-	private HashMap<Player,Player> channels = new HashMap<Player,Player>();
-	
-	public void PrivateMessageHandler(Player player1,Player player2, String argsmessage){
-		player1.sendMessage(ChatColor.RED+"To "+player2.getDisplayName()+": "+ argsmessage);
-		player2.sendMessage(ChatColor.RED+"From "+player1.getName()+": "+ argsmessage);
+	private FileConfiguration config;
+	private int chatmax = 1000;
+	private HashMap<String, String> channels = new HashMap<String, String>();
+
+	public ChatManager (FileConfiguration configInstance) {
+		config = configInstance;
+		config.getInt("chat.range", 1000);
 	}
-	public void PlayerBroadcast(Player player, String message, Set<Player> recievers){
+
+	public void sendPrivateMessage(Player from, Player to, String message) {
+		from.sendMessage(ChatColor.DARK_PURPLE + "To " + to.getDisplayName() + ": " + message);
+		to.sendMessage(ChatColor.DARK_PURPLE + "From " +from.getName() + ": " + message);
+	}
+	
+	public void sendPlayerBroadcast(Player player, String message, Set<Player> receivers) {
 		Location location = player.getLocation();
 		int x = location.getBlockX();
 		int y = location.getBlockY();
 		int z = location.getBlockZ();
-		double chatrange=0;
-		for (Player reciever: recievers){
+		double chatrange = 0;
+		
+		boolean received = false;
+
+		for (Player receiver : receivers) {
 			
-			Long distance = Math.round(Math.sqrt(((x-reciever.getLocation().getBlockX())^2 + (z-reciever.getLocation().getBlockZ())^2)));
-			int height=y-reciever.getLocation().getBlockY();
-			int extradistance=0;
-			int sign=0;
-			if (height<0){sign=-1;}
-			if (height>0){sign=1;}
+			int distX = x - receiver.getLocation().getBlockX();
+			distX *= distX;
+			int distZ = z - receiver.getLocation().getBlockZ();
+			distZ *= distZ;
 			
-				for (int i=0; i<=Math.abs(height);i++){ 
-					extradistance +=4;
-				}
-				if (sign==-1){
-				chatrange= distance+extradistance;
+			long distance = Math.round(Math.sqrt((distX + distZ)));
+			int height = y - receiver.getLocation().getBlockY();
+			int extradistance = 0;
+			int sign = 0;
+
+			if (height < 0)
+				sign = -1;
+			if (height > 0)
+				sign = 1;
+
+			for (int i = 0; i <= Math.abs(height); i++) { 
+				extradistance += 4;
 			}
-				if (sign==1){
-				chatrange=distance-extradistance;
-				}
-				if (reciever==player || recievers.size()<1){
-					player.sendMessage("No one can hear you");
-					continue;
-				}
-				if (chatrange<=chatmax){
-			reciever.sendMessage(ChatColor.RED+player.getDisplayName()+": "+message+chatrange);
+			
+			if (sign == -1) {
+				chatrange = distance + extradistance;
+			}
+			
+			if (sign == 1) {
+				chatrange = distance - extradistance;
+			}
+			
+			if (chatrange <= chatmax) {
+				receiver.sendMessage(player.getDisplayName() + ": " + message);
+			}
+			
+			if(!received && !receiver.getName().equals(player.getName())) {
+				received = true;
+			}
+			
+			if(!received)
+				player.sendMessage(ChatColor.YELLOW + "No one hears you.");
+			// add when a a little bit above a certain range it removes letters
 		}
-				// add when a a little bit above a certain range it removes letters
-				
 	}
+	public void addChannel(String player1, String player2) {
+		if (getChannel(player1) != null) {
+			removeChannel(player1);
+			channels.put(player1, player2);
+		}
+		else {
+			channels.put(player1, player2);
+		}
 	}
-	 public void addChannel(Player player1, Player player2){
-	 
-	 if (getChannel(player1) != null){
-	 
-	 removeChannel(player1);
-	 channels.put(player1, player2);
-	 }
-	 else{
-	 
-	 channels.put(player1, player2);
-	 	}
-	 }
-	 public Player getChannel(Player player){	 
-		 if (channels.containsKey(player)){	 
-			 return channels.get(player);
-		 }
-		 else{
-			 return null;
-		 }
-	 }
-	 public void removeChannel (Player player){
-	 
-	 if (channels.containsKey(player))
-	 {
-	 channels.remove(player);
-	 }
-	 }
+	
+	public String getChannel(String player) {	 
+		if (channels.containsKey(player)) {	 
+			return channels.get(player);
+		}
+		else {
+			return null;
+		}
+	}
+	
+	public void removeChannel(String player) {
+		if (channels.containsKey(player)) {
+			channels.remove(player);
+		}
+	}
 
 }
--- src/com/untamedears/civchat/Commands.java
+++ src/com/untamedears/civchat/Commands.java
@@ -7,84 +7,104 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class Commands implements CommandExecutor{
-	CivChat civ;
-	ChatManager chatManager;
-	public Commands (ChatManager chatManagerInstance){
-	chatManager = chatManagerInstance;
+public class Commands implements CommandExecutor {
+	private CivChat civ;
+	private ChatManager chatManager;
+	
+	public Commands (ChatManager chatManagerInstance) {
+		chatManager = chatManagerInstance;
 	}
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
-		
-             
-		if (label.equalsIgnoreCase("tell") || label.equalsIgnoreCase("message")){
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if (command.getName().equals("tell")) {
 			if (!(sender instanceof Player))
 			{
-			sender.sendMessage("You have to be a player to use that command!");
+				sender.sendMessage("You have to be a player to use that command!");
 			}
+			
 			Player player = (Player) sender;
-			
+
 			if (args.length < 1) {
-				if (chatManager.getChannel(player)==null){
-					player.sendMessage("Provide a player name");
+				if (chatManager.getChannel(player.getName()) == null) {
+					player.sendMessage(ChatColor.RED + "Usage: /tell <player>");
 				}
-				else{
-				chatManager.removeChannel(player);
-					player.sendMessage("In Global Channel");
+				else {
+					chatManager.removeChannel(player.getName());
+					player.sendMessage(ChatColor.YELLOW + "You have moved to regular chat.");
 				}
-                return true;
-        }
-			else if(args.length == 1){
-				Player playerreciever= Bukkit.getPlayerExact(args[0]);
-				if (playerreciever==null){
-					player.sendMessage("Player is offline");
-					return true;
+				
+				return true;
 			}
-				else{
-					chatManager.addChannel(player, playerreciever);
-					player.sendMessage("In Channel with "+ playerreciever.getDisplayName());
+			else if(args.length == 1) {
+				Player receiver = Bukkit.getPlayerExact(args[0]);
+				
+				if (receiver == null) {
+					player.sendMessage(ChatColor.RED + "Error: Player is offline.");
+					
 					return true;
+				}
+				else {
+					chatManager.addChannel(player.getName(), receiver.getName());
+					player.sendMessage(ChatColor.YELLOW + "You are now chatting with "+ receiver.getDisplayName() + ".");
+					
+					return true;
+				}
+			}
+
+			if (args.length > 1) {
+				Player receiver = Bukkit.getPlayerExact(args[0]);
+
+				if (receiver == null) {
+					sender.sendMessage(ChatColor.RED + "Error: Player is offline.");
+					
+					return true;
+				}
+				
+				else {
+					StringBuilder message = new StringBuilder();
+					
+					for (int i = 1; i < args.length; i++) {
+						message.append(args[i]);
+						
+						if(i < args.length - 1)
+							message.append(" ");
 					}
-			}
-			
-			if (args.length>1){
-				Player playerreciever= Bukkit.getPlayerExact(args[0]);
-				
-				if (playerreciever==null){
-					sender.sendMessage("Player is offline");
+
+					chatManager.sendPrivateMessage(player, receiver, message.toString());
+
 					return true;
 				}
-				else{
-				StringBuilder argsmessage= new StringBuilder();
-				for (int i= 1;i<args.length;i++){
-					argsmessage.append(args[i]);
-					argsmessage.append(" ");
-				}
-				
-				
-				chatManager.PrivateMessageHandler(player, playerreciever, argsmessage.toString());
+			}
 			
 			return true;
-				}
-			}
-			return true;
 		}
-		if (label.equalsIgnoreCase("civchat")){
-			if (args[0].equals("save")){
-				sender.sendMessage("saved config");
+		
+		if (command.getName().equals("civchat")) {
+			if(!sender.hasPermission("civchat.admin")) {
+				sender.sendMessage(ChatColor.RED + "You do not have permission to do this.");
+				return true;
+			}
+			
+			if (args[0].equals("save")) {
+				sender.sendMessage(ChatColor.GREEN + "Saved configuration.");
 				civ.saveConfig();
+				
 				return true;
 			}
-			if (args[0].equals("reload")){
-				sender.sendMessage("reloaded config");
-				civ.ReloadConfig();
+			if (args[0].equals("reload")) {
+				sender.sendMessage(ChatColor.GREEN + "Reloaded configuration.");
+				civ.reloadConfig();
+				
 				return true;
 			}
-			else{ sender.sendMessage("Incorrect arg");}
+			
+			sender.sendMessage(ChatColor.RED + "Usage: /civchat <save/reload>");
+			
 			return true;
 		}
+		
 		return true;
 	}
-	
+
 }
 
 
