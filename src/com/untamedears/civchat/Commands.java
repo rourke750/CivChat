a01f06eb00ea48a2a1dc0ea14bc95062be91602b
rourke750 <rourke7501@gmail.com>
2013-07-14 12:07:51 -0400

    Update Commands.java

--- src/com/untamedears/civchat/Commands.java
+++ src/com/untamedears/civchat/Commands.java
@@ -254,7 +254,7 @@ public class Commands implements CommandExecutor {
                 }
             	else{
             		int i=0;
-            		if (chatManager.getIgnoreList(player)==null){
+            		if (chatManager.getIgnoreList(player)!=null){
             			chatManager.setIgnoreList(player, reciever); // something wrong with this line.  Keeps sending null?
             			return true;
             		}
